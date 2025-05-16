c1f619fc901680cc8892d8beaaa5c754adde8cbc
Ryan Taylor <rctaylor123@gmail.com>
2025-05-15 20:42:09 -0600

    update imports with root path

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,5 +1,5 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
+import db from "@/db";
+import { advocates } from "@/db/schema";
 
 export async function GET() {
   const data = await db.select().from(advocates);
