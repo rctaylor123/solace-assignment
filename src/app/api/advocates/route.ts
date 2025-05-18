d98f73ef8256de5d4777d4a3ed1a2016c280d2e5
Ryan Taylor <rctaylor123@gmail.com>
2025-05-18 15:15:10 -0600

    move filtering and pagination logic to backend

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,39 @@
 import db from '@/db';
+import { ilike, or, sql, asc } from 'drizzle-orm';
+import { PgSelectQueryBuilder } from 'drizzle-orm/pg-core';
+import { NextRequest } from 'next/server';
+
 import { advocates } from '@/db/schema';
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const search = request.nextUrl.searchParams.get('search') || '';
+  const page = parseInt(request.nextUrl.searchParams.get('page') || '1');
+  const pageSize = parseInt(request.nextUrl.searchParams.get('pageSize') || '5');
 
-  return Response.json({ data });
+  let query = db.select().from(advocates).orderBy(asc(advocates.lastName), asc(advocates.firstName)).$dynamic();
+  if (search && search.length > 0) {
+    query = query.where(buildWhere(query, search));
+  }
+
+  const totalCount = await db.$count(query); // count the total number of records before pagination
+  query = withPagination(query, page, pageSize);
+  const resp = await query;
+  return Response.json({ advocates: resp, totalCount: totalCount });
+}
+
+function buildWhere(query: PgSelectQueryBuilder, search: string) {
+  return or(
+    ilike(advocates.firstName, `%${search}%`),
+    ilike(advocates.lastName, `%${search}%`),
+    ilike(advocates.city, `%${search}%`),
+    ilike(advocates.degree, `%${search}%`),
+    sql`${advocates.specialties}::text ilike ${`%${search}%`}`,
+    sql`${advocates.phoneNumber}::text ilike ${`%${search}%`}`,
+    sql`${advocates.yearsOfExperience}::text ilike ${`%${search}%`}`,
+    sql`concat(${advocates.firstName}, ' ', ${advocates.lastName}) ilike ${`%${search}%`}`
+  );
+}
+
+function withPagination(query: PgSelectQueryBuilder, page: number, pageSize: number) {
+  return query.limit(pageSize).offset((page - 1) * pageSize);
 }
--- src/app/components/data-table.tsx
+++ src/app/components/data-table.tsx
@@ -27,41 +27,48 @@ export default function DataTable({ filteredAdvocates, isLoading }: DataTablePro
   }
 
   return (
-    <table className='min-w-full divide-y divide-gray-200'>
-      <thead className='bg-gray-50'>
-        <tr>
-          {tableHeaders.map((header: string, i: number) => (
-            <th key={i} scope='col' className='px-6 py-3 text-start'>
-              <div className='flex items-center gap-x-2'>
-                <span className='text-xs font-semibold text-gray-800 uppercase'>{header}</span>
-              </div>
-            </th>
-          ))}
-        </tr>
-      </thead>
+    <>
+      <table className='min-w-full divide-y divide-gray-200'>
+        <thead className='bg-gray-50'>
+          <tr>
+            {tableHeaders.map((header: string, i: number) => (
+              <th key={i} scope='col' className='px-6 py-3 text-start'>
+                <div className='flex items-center gap-x-2'>
+                  <span className='text-xs font-semibold text-gray-800 uppercase'>{header}</span>
+                </div>
+              </th>
+            ))}
+          </tr>
+        </thead>
 
-      <tbody className='divide-y divide-gray-200'>
-        {filteredAdvocates.map((advocate, i) => {
-          return (
-            <tr key={i}>
-              <TableCell>{advocate.firstName}</TableCell>
-              <TableCell>{advocate.lastName}</TableCell>
-              <TableCell>{advocate.city}</TableCell>
-              <TableCell>{advocate.degree}</TableCell>
-              <TableCell>
-                <ul className='list-disc space-y-2 ps-5 text-sm text-gray-600 marker:text-green-800'>
-                  {advocate.specialties.map((s: string, i: number) => (
-                    <li key={i}>{s}</li>
-                  ))}
-                </ul>
-              </TableCell>
-              <TableCell>{advocate.yearsOfExperience}</TableCell>
-              <TableCell>{advocate.phoneNumber}</TableCell>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+        <tbody className='divide-y divide-gray-200'>
+          {filteredAdvocates.map((advocate, i) => {
+            return (
+              <tr key={i}>
+                <TableCell>{advocate.firstName}</TableCell>
+                <TableCell>{advocate.lastName}</TableCell>
+                <TableCell>{advocate.city}</TableCell>
+                <TableCell>{advocate.degree}</TableCell>
+                <TableCell>
+                  <ul className='list-disc space-y-2 ps-5 text-sm text-gray-600 marker:text-green-800'>
+                    {advocate.specialties.map((s: string, i: number) => (
+                      <li key={i}>{s}</li>
+                    ))}
+                  </ul>
+                </TableCell>
+                <TableCell>{advocate.yearsOfExperience}</TableCell>
+                <TableCell>{advocate.phoneNumber}</TableCell>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {filteredAdvocates.length == 0 && (
+        <div className='m-5 flex items-center justify-center'>
+          <span className='text-sm text-gray-600'>No advocates found</span>
+        </div>
+      )}
+    </>
   );
 }
 
